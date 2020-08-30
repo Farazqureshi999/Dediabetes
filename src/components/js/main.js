538a35e05b82ec55f01c1e0f1f5d69b2b0b67d97
farazqureshi999 <blackhat786@hotmail.com>
2020-08-30 22:12:26 +0500

    main.js fix

--- src/components/js/main.js
+++ src/components/js/main.js
@@ -1,9 +1,9 @@
 import $ from 'jquery';
 
 
-$(document).ready(function(){
-    $( ".btn12" ).click(function() {
-        $(this).toggleClass("open");
-        $(".mobile-header-menu").toggleClass("open-menu");
-      });
-})
+// $(document).ready(function(){
+//     $( ".btn12" ).click(function() {
+//         $(this).toggleClass("open");
+//         $(".mobile-header-menu").toggleClass("open-menu");
+//       });
+// })
