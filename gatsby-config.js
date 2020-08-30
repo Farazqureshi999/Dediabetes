c5e6c5f2e43ede5b5a3752d20fd3b26e5291d836
farazqureshi999 <blackhat786@hotmail.com>
2020-08-30 23:54:28 +0500

    image folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'image',
-        path: `${__dirname}/static/assets`,
+        path: `${__dirname}/static/image`,
       },
     },
     {
