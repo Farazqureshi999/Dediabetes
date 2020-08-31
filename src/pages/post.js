96940beb235426582a1e81fb027506d556f62811
farazqureshi999 <blackhat786@hotmail.com>
2020-08-31 13:55:18 +0500

    config.yml settings

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,35 +1,15 @@
-// import React from 'react'
-// import Layout from '../components/layout'
-// import img from '../images/main.jpg'
-// import { graphql } from 'gatsby';
+import React from 'react'
+import Layout from '../components/layout'
 
-// export const query = graphql`
-//   query {
-//     allFile (filter: {sourceInstanceName: {eq: "content"} name: {eq: "home"}}) {
-//       edges {
-//         node {
-//           childMarkdownRemark {
-//             frontmatter {
-//               title
-//               intro
-//               image
-//           }
-//         }
-//       }
-//     }
-//   }
-// }`
 
-// function post(props) {
-//     const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter;
-//     return (
+function post(props) {
+    
+    return (
         
-//         <Layout>
-//          <h1>{data.title}</h1>
-//       <p>{data.intro}</p>
-//       <img src={data.image} alt="jjj"/>
-//         </Layout>
-//     )
-// }
+        <Layout>
+         hello
+        </Layout>
+    )
+}
 
-// export default post
+export default post
