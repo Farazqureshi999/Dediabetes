148c8d6b8a0b1b55a475486337299a3a96b7402e
farazqureshi999 <blackhat786@hotmail.com>
2020-08-31 13:52:11 +0500

    config.yml settings

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,24 @@
-// create pages dynamically 
-const path = require('path');
+// // create pages dynamically 
+// const path = require('path');
 
-exports.createPages = async ({graphql,actions}) =>{
-   const {createPage} = actions;
-   const result = await graphql(`
-   query getAllProducts{
-    products:allContentfulProduct{
-      nodes{
-        slug
-      }
-    }
-  }
-   `)
-   result.data.products.nodes.forEach(product => {
-       createPage({
-           path : `/products/${product.slug}`,
-           component: path.resolve('src/templates/blog-post.js'),
-           context: {
-               slug : product.slug
-           }
-       })
-   });
-}
+// exports.createPages = async ({graphql,actions}) =>{
+//    const {createPage} = actions;
+//    const result = await graphql(`
+//    query getAllProducts{
+//     products:allContentfulProduct{
+//       nodes{
+//         slug
+//       }
+//     }
+//   }
+//    `)
+//    result.data.products.nodes.forEach(product => {
+//        createPage({
+//            path : `/products/${product.slug}`,
+//            component: path.resolve('src/templates/blog-post.js'),
+//            context: {
+//                slug : product.slug
+//            }
+//        })
+//    });
+// }
